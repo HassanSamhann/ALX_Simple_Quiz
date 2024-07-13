ba2db799aeb5319ccd443af823d3be2e40c9af58
hassan <hassan700019@gmail.com>
2024-07-13 21:26:58 +0300

    sdsd

--- quiz.js
+++ quiz.js
@@ -1,11 +1,11 @@
+let selectedOption = document.querySelector("input[name='quiz']:checked");
+let  userAnswer = selectedOption.value ;
 
 function  checkAnswer() {
     let correctAnswer  = "4" ; 
-    let selectedOption = document.querySelector("input[name='quiz']:checked");
-   let  userAnswer = selectedOption.value ;
     
 
-    if(userAnswer == correctAnswer){
+    if(userAnswer === correctAnswer){
         document.getElementById("feedback").textContent = "Correct! Well done." ; 
 
     }else{
